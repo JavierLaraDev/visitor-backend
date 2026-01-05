c292479ca9ac2501f56ce1f9a53b0a752e064c79
Javier Lara <javierlarasolis1414@gmail.com>
2026-01-05 16:42:01 -0400

    Run database migrations for visitors module

--- src/routes/visitorRoutes.ts
+++ src/routes/visitorRoutes.ts
@@ -1,14 +1,15 @@
 
 import express from 'express';
 import { verifyRole, verifyToken } from '../middlewares/auth.middleware.js';
-import { createVisitor, deleteVisitor, getVisitor, getVisitors, updateVisitor } from '../controllers/visitorController.js';
+import { createVisitor, deleteVisitor, getVisitor, getVisitors, updateVisitor, updateVisitorStatus } from '../controllers/visitorController.js';
 
 const router=express.Router();
 
 router.post('/',verifyToken,verifyRole('PASANTE'), createVisitor);
-router.get('/',verifyToken,verifyRole('PASANTE'), getVisitors);
+router.get('/',verifyToken,verifyRole('PASANTE','ADMIN'), getVisitors);
 router.get('/:id',verifyToken,verifyRole('PASANTE'), getVisitor);
 router.put('/:id',verifyToken,verifyRole('PASANTE'),updateVisitor );
 router.delete('/:id',verifyToken,verifyRole('PASANTE'), deleteVisitor);
+router.patch('/:id/status', verifyToken, verifyRole('ADMIN'), updateVisitorStatus);
 
 export default router;
